a67ab6545d0f13c572f720a264c11628e356c62b
Adrian Webb <adrian@webb.sh>
2019-03-21 12:25:34 -0400

    Updates to the install script (updated Helm version and formatting changes).

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,10 +4,8 @@
 #
 apt-key adv --fetch-keys https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+apt-get install -y kubectl && rm -rf /var/lib/apt/lists/*
 
-apt-get install -y kubectl \
-    && rm -rf /var/lib/apt/lists/*
-
-curl -o /tmp/helm_install.sh https://raw.githubusercontent.com/helm/helm/v2.9.1/scripts/get \
-    && chmod 700 /tmp/helm_install.sh
-/tmp/helm_install.sh && rm -f /tmp/helm_install.sh
+curl -o helm_install.sh https://raw.githubusercontent.com/helm/helm/v2.13.0/scripts/get \
+    && chmod 700 helm_install.sh
+./helm_install.sh && rm -f helm_install.sh
