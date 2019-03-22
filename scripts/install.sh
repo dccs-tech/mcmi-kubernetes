e1973ad1765c5a3c5661e5a72d0659977ad2f013
Adrian Webb <adrian@webb.sh>
2019-03-22 02:46:50 -0400

    Fixing the helm install process and updating helm version in the install script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,10 +2,12 @@
 #
 # Install Kubernetes related packaging
 #
+set -e
+
 apt-key adv --fetch-keys https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 apt-get install -y kubectl && rm -rf /var/lib/apt/lists/*
 
-curl -o helm_install.sh https://raw.githubusercontent.com/helm/helm/v2.13.0/scripts/get \
-    && chmod 700 helm_install.sh
-./helm_install.sh && rm -f helm_install.sh
+curl -o helm_install.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get
+chmod 700 helm_install.sh
+./helm_install.sh -v v2.13.0
