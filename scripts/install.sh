bf88591feb8eebb3c8c89a6326da3f78a3752bd7
Adrian Webb <adrian@webb.sh>
2019-03-23 05:51:07 -0400

    Minor splitting of commands in the install script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,8 @@ set -e
 apt-key adv --fetch-keys https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 apt-get update
-apt-get install -y kubectl && rm -rf /var/lib/apt/lists/*
+apt-get install -y kubectl
+rm -rf /var/lib/apt/lists/*
 
 curl -o helm_install.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get
 chmod 700 helm_install.sh
