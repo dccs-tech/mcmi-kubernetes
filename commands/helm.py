dead9139425761ec6381b349bd8ea7fe80767a67
Adrian Webb <adrian@webb.sh>
2020-06-24 08:22:02 -0400

    Using the kube_exec command mixin method in the kubectl and helm commands.

--- commands/helm.py
+++ commands/helm.py
@@ -7,26 +7,16 @@ import os
 class Helm(Command('helm')):
 
     def exec(self):
-        config_path = os.path.join(self.manager.data_dir, '.kube')
-        helm_path = os.path.join(self.manager.data_dir, '.helm')
-
-        with filesystem_dir(helm_path) as home_dir:
+        with filesystem_dir(os.path.join(self.manager.data_dir, '.helm')) as home_dir:
             home_dir.mkdir('cache')
             home_dir.mkdir('config')
             home_dir.mkdir('data')
 
-            command = ['helm'] + self.options.get('args', [])
-            success = self.sh(
-                command,
+            self.kube_exec(home_dir, 'helm',
                 env = {
-                    "KUBECONFIG": os.path.join(config_path, 'config'),
                     "HELM_HOME": home_dir.base_path,
                     "XDG_CACHE_HOME": home_dir.path('cache'),
                     "XDG_CONFIG_HOME": home_dir.path('config'),
                     "XDG_DATA_HOME": home_dir.path('data')
-                },
-                cwd = self.manager.module_dir,
-                display = True
+                }
             )
-            if not success:
-                self.error("Helm command failed: {}".format(" ".join(command)))
--- commands/kubectl.py
+++ commands/kubectl.py
@@ -1,23 +1,11 @@
 from systems.commands.index import Command
 from utility.temp import temp_dir
 
-import os
-
 
 class Kubectl(Command('kubectl')):
 
     def exec(self):
         with temp_dir() as temp:
-            config_path = os.path.join(self.manager.data_dir, '.kube')
-
-            command = ['kubectl', '--insecure-skip-tls-verify=true'] + self.options.get('args', [])
-            success = self.sh(
-                command,
-                env = {
-                    "KUBECONFIG": os.path.join(temp.link(config_path), 'config')
-                },
-                cwd = self.manager.module_dir,
-                display = True
+            self.kube_exec(temp, 'kubectl',
+                options = ['--insecure-skip-tls-verify=true']
             )
-            if not success:
-                self.error("Kubectl command failed: {}".format(" ".join(command)))
