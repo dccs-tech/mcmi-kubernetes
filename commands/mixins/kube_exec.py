802926b386274a83f065bc4bbcb2d8d8ace06ced
Adrian Webb <adrian@webb.sh>
2020-06-24 08:19:23 -0400

    Adding an initial kube_exec command mixin implementation.

---
+++ commands/mixins/kube_exec.py
@@ -0,0 +1,33 @@
+from systems.commands.index import CommandMixin
+from utility.data import ensure_list
+
+import os
+
+
+class KubeExecMixin(CommandMixin('kube_exec')):
+
+    def kube_exec(self, filesystem, executable, options = None, env = None):
+        args = self.options.get('args', [])
+        if options is None:
+            options = []
+
+        if options:
+            command = [ executable ] + ensure_list(options) + args
+        else:
+            command = [ executable ] + args
+
+        config_path = os.path.join(self.manager.data_dir, '.kube')
+        command_env = {
+            "KUBECONFIG": os.path.join(filesystem.link(config_path), 'config')
+        }
+        if env and isinstance(env, dict):
+            command_env = { **command_env, **env }
+
+        success = self.sh(
+            command,
+            env = command_env,
+            cwd = self.manager.module_dir,
+            display = True
+        )
+        if not success:
+            self.error("Command {} failed: {}".format(executable, " ".join(command)))
