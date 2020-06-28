b1c54f8cbcde9204434f20d9a2286cdcbe7bdca8
Adrian Webb <adrian@webb.sh>
2020-06-27 22:31:37 -0400

    Updating the GPG key addition in the install script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@
 #
 set -e
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - 2>/dev/null 1>&2
+wget -qO - https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - 2>/dev/null 1>&2
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubectl
