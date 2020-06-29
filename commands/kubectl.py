80ce427196ed4b10198558224b0999e18992cd9d
Adrian Webb <adrian@webb.sh>
2020-06-28 21:50:14 -0400

    Removing the insecure-skip-tls-verify flag in the kubectl command.

--- commands/kubectl.py
+++ commands/kubectl.py
@@ -6,6 +6,4 @@ class Kubectl(Command('kubectl')):
 
     def exec(self):
         with temp_dir() as temp:
-            self.kube_exec(temp, 'kubectl',
-                options = ['--insecure-skip-tls-verify=true']
-            )
+            self.kube_exec(temp, 'kubectl')
