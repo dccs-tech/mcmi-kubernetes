ebd3456fd6a1f40022166094448df5395baa6c01
Adrian Webb <adrian@webb.sh>
2020-07-04 22:30:14 -0400

    Adding installation of the istioctl command in the install script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,3 +13,12 @@ rm -rf /var/lib/apt/lists/*
 curl -o helm_install.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get 2>/dev/null
 chmod 700 helm_install.sh
 ./helm_install.sh -v v3.2.4
+
+ISTIO_VERSION=1.6.4
+
+if [ ! -f /usr/local/bin/istioctl ]
+then
+    wget https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-linux-amd64.tar.gz 2>/dev/null
+    tar -xzf istio-${ISTIO_VERSION}-linux-amd64.tar.gz
+    cp -f istio-${ISTIO_VERSION}/bin/istioctl /usr/local/bin
+fi
